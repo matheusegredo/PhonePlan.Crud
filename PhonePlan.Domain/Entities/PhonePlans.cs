bc113c91fe27d842ca3cf81907865453fddb2746
Matheus Segredo <msegredo@onlineapp.com.br>
2021-10-09 13:30:59 -0300

    Adicionando as entidades e enumeradores

---
+++ PhonePlan.Domain/Entities/PhonePlans.cs
@@ -0,0 +1,17 @@
+﻿namespace PhonePlan.Domain.Entities
+{
+	public sealed class PhonePlans
+	{
+		public int PlanCode { get; set; }
+
+		public int Minutes { get; set; }
+
+		public string InternetFranchise { get; set; }
+
+		public decimal Value { get; set; }
+
+		public PlanType PlanType { get; set; }
+
+		public string Operator { get; set; }
+	}
+}
---
+++ PhonePlan.Domain/Enums/PlanType.cs
@@ -0,0 +1,9 @@
+﻿namespace PhonePlan.Domain.Entities
+{
+	public enum PlanType
+	{ 
+		Controle = 1,
+		Pos = 2,
+		Pre = 3
+	}
+}
