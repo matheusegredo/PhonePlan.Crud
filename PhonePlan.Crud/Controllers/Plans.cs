238c6498a2df06df9eedc8d6a814e794c47e7a36
Matheus Segredo <msegredo@onlineapp.com.br>
2021-10-09 13:06:29 -0300

    Adicionar arquivos de projeto.

---
+++ PhonePlan.Crud/Controllers/Plans.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhonePlan.Crud.Controllers
+{
+	[ApiController]
+	[Route("api/plans")]
+	public class Plans : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public Plans(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		
+	}
+}
