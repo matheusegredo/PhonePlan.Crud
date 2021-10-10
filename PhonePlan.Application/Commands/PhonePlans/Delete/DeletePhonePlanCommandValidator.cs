460d1526b70b8ce86fc07cdf4aa22fff17366566
Matheus Segredo <msegredo@onlineapp.com.br>
2021-10-10 19:09:08 -0300

    Adicionando validator para o command delete e para query search

--- PhonePlan.Application/Commands/PhonePlans/Delete/DeletePhonePlanCommandValidator.cs
+++ PhonePlan.Application/Commands/PhonePlans/Delete/DeletePhonePlanCommandValidator.cs
@@ -1,6 +1,13 @@
-﻿namespace PhonePlan.Application.Commands.PhonePlans.Delete
+﻿using FluentValidation;
+
+namespace PhonePlan.Application.Commands.PhonePlans.Delete
 {
-	public sealed class DeletePhonePlanCommandValidator
+	public sealed class DeletePhonePlanCommandValidator : AbstractValidator<DeletePhonePlanCommand>
 	{
+		public DeletePhonePlanCommandValidator()
+		{
+			RuleFor(p => p.PlanCode)
+				.NotEmpty();
+		}
 	}
 }
---
+++ PhonePlan.Application/Queries/PhonePlans/Search/SearchPhonePlansQueryValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Collections.Generic;
+
+namespace PhonePlan.Application.Queries.PhonePlans.Search
+{
+	public class SearchPhonePlansQueryValidator : AbstractValidator<SearchPhonePlansQuery>
+	{		
+	}
+}
