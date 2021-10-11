83eebec56758e12ed418dac3a609972b649094b2
Matheus Segredo <msegredo@onlineapp.com.br>
2021-10-10 22:09:35 -0300

    Atualização do Profile com novas entidades

--- PhonePlan.Application/Profiles/PhonePlansProfiles.cs
+++ PhonePlan.Application/Profiles/PhonePlansProfiles.cs
@@ -11,9 +11,18 @@ namespace PhonePlan.Application.Profiles
 	{
 		public PhonePlansProfiles()
 		{
-			CreateMap<PostPhonePlanCommand, PhonePlansEntity>();
+			CreateMap<PostPhonePlanDataCommand, PhonePlansEntity>();
+
+			CreateMap<PostPhonePlanCommand, DirectRemoteDialingEntity>()
+				.ForMember(dest => dest.Code, o => o.MapFrom(src => src.DDD));
 
 			CreateMap<PutPhonePlanCommand, PhonePlansEntity>();
+
+			CreateMap<DirectRemoteDialingPhonePlanEntity, SearchPhonePlansQueryResponse>()
+				.ForMember(dest => dest.DDD, o => o.MapFrom(src => src.DirectRemoteDialing.Code))
+				.IncludeMembers(p => p.PhonePlans);
+
+			CreateMap<DirectRemoteDialingEntity, SearchPhonePlansQueryResponse>();				
 
 			CreateMap<PhonePlansEntity, SearchPhonePlansQueryResponse>()
 				.ForMember(dest => dest.PlanType, o => o.MapFrom(src => Enum.GetName(src.PlanType)));
