4214ae8ef9f13066caa5c1964d87a45be6d3717e
Matheus Segredo <msegredo@onlineapp.com.br>
2021-10-11 09:18:47 -0300

    Removendo construtor não utilizado

--- PhonePlan.Application/Commands/PhonePlans/Delete/DeletePhonePlanCommand.cs
+++ PhonePlan.Application/Commands/PhonePlans/Delete/DeletePhonePlanCommand.cs
@@ -4,11 +4,6 @@ namespace PhonePlan.Application.Commands.PhonePlans.Delete
 {
 	public sealed class DeletePhonePlanCommand : IRequest<Unit>
 	{
-		public DeletePhonePlanCommand(int planCode)
-		{
-			PlanCode = planCode;
-		}
-
 		public int PlanCode { get; set; }
 
 		public string DDD { get; set; }
