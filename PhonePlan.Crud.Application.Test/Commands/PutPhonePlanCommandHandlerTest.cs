57ed2b50e6c0a750484d6408b449f68090565b81
Matheus Segredo <msegredo@onlineapp.com.br>
2021-10-11 19:19:15 -0300

    Teste unitário do méotodo put adicionado

--- PhonePlan.Crud.Application.Test/Commands/PutPhonePlanCommandHandlerTest.cs
+++ PhonePlan.Crud.Application.Test/Commands/PutPhonePlanCommandHandlerTest.cs
@@ -2,7 +2,6 @@
 using MediatR;
 using PhonePlan.Application.Commands.PhonePlans.Put;
 using PhonePlan.Application.Profiles;
-using PhonePlan.Application.Queries.PhonePlans.Search;
 using PhonePlan.CrossCutting.Exceptions;
 using PhonePlan.Domain.Entities;
 using System.Threading.Tasks;
@@ -11,16 +10,46 @@ using Xunit;
 namespace PhonePlan.Crud.Application.Test.Commands
 {
 	[Trait("Command", "PhonePlan")]
-	public sealed class SearchPhonePlansQueryHandlerTest
+	public sealed class PutPhonePlanCommandHandlerTest
 	{
-		private readonly SearchPhonePlansQueryHandler _handler;
+		private readonly PutPhonePlanCommandHandler _handler;
 
-		public SearchPhonePlansQueryHandlerTest()
+		public PutPhonePlanCommandHandlerTest()
 		{
 			var mapper = new MapperConfiguration(cfg => cfg.AddProfile(new PhonePlansProfiles())).CreateMapper();
 			var context = ApplicationDbContextFactory.Create();
 
-			_handler = new SearchPhonePlansQueryHandler(mapper, context);
-		}		
+			_handler = new PutPhonePlanCommandHandler(mapper, context);
+		}
+
+		[Fact(DisplayName = "Updating an existing phone plan should return unit")]
+		public async Task PutPhonePlanCommandHandler_UpdateAnExistingPhonePlan_ShouldReturnUnit()
+		{
+			var command = new PutPhonePlanCommand
+			{
+				PlanCode = 1,
+				Minutes = 0,
+				InternetFranchise = string.Empty,
+				Value = 0.00M,
+				PlanType = PlanType.Controle
+			};
+
+			var response = await _handler.Handle(command, default);
+
+			Assert.IsType<Unit>(response);
+		}
+
+		[Fact(DisplayName = "Updating an nonexisting phone plan should return unit")]
+		public async Task PutPhonePlanCommandHandler_UpdateAnNonExistingPhonePlan_ShouldThrowsNotFoundException()
+		{
+			var command = new PutPhonePlanCommand
+			{
+				PlanCode = 999,				
+			};
+
+			var response = await 
+
+			Assert.ThrowsAsync<NotFoundException>(() => _handler.Handle(command, default));
+		}
 	}
 }
